1a0bcf20583417058e70bfb94ad5e55a545c1750
Nalin Kularathna <pgnaleen@gmail.com>
2022-11-03 01:52:19 +0800

    decryption

--- src/main/java/com/dbs/paynowpremiumcollection/service/impl/EncryptionServiceImpl.java
+++ src/main/java/com/dbs/paynowpremiumcollection/service/impl/EncryptionServiceImpl.java
@@ -5,6 +5,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPException;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchProviderException;
 import java.security.Security;
@@ -18,5 +19,10 @@ public class EncryptionServiceImpl implements EncryptionService {
         PGPFileEncryptionServiceImpl.encryptFile("output_encrypted_file.asc",
                 "src/main/resources/Capture.PNG",
                 "src/main/resources/0xD89B5951-pub.asc", true, false);
+
+        PGPFileEncryptionServiceImpl.decryptFile("output_encrypted_file.asc",
+                "src/main/resources/0xD89B5951-sec.asc",
+                "12345".toCharArray(),
+                new File("no_need_name_as_encrypted_file_has_name").getName());
     }
 }
